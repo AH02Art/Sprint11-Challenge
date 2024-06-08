98261f1d3f7d2ba60a873c1b7688fc77cca889f5
Alex Handley <smithvillescholarsah@gmail.com>
2024-06-07 21:58:01 -0500

    Sprint11 Challenge completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,7 @@ import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import axios from "axios";
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
@@ -18,22 +19,38 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { /* ✨ implement */ navigate("/"); }
+  const redirectToArticles = () => { /* ✨ implement */ navigate("/articles"); }
 
   const logout = () => {
     // ✨ implement
     // If a token is in local storage it should be removed,
+    localStorage.removeItem("token");
     // and a message saying "Goodbye!" should be set in its proper state.
+    setMessage("Goodbye!");
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    redirectToLogin();
   }
 
   const login = ({ username, password }) => {
     // ✨ implement
     // We should flush the message state, turn on the spinner
+    setMessage("");
+    setSpinnerOn(true);
     // and launch a request to the proper endpoint.
     // On success, we should set the token to local storage in a 'token' key,
+    axios.post(loginUrl, { username, password })
+    .then((response) => {
+      console.log("login data: ", response);
+      const token = response.data.token;
+        localStorage.setItem("token", token)
+        redirectToArticles();
+    })
+    .catch((error) => { 
+      console.error(error);
+    })
+    .finally(() => setSpinnerOn(false));
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
   }
@@ -41,8 +58,21 @@ export default function App() {
   const getArticles = () => {
     // ✨ implement
     // We should flush the message state, turn on the spinner
+    setMessage("");
+    setSpinnerOn(true);
     // and launch an authenticated request to the proper endpoint.
-    // On success, we should set the articles in their proper state and
+   axios.get(articlesUrl, { headers: { Authorization: localStorage.getItem("token") }})
+    .then((response) => {
+      console.log("get request: ", response);
+      setArticles(response.data.articles);
+      setMessage(response.data.message);
+    })
+    .catch((error) => {
+      console.log(error);
+      localStorage.removeItem("token");
+      redirectToLogin();
+    })
+    .finally(() => setSpinnerOn(false))
     // put the server success message in its proper state.
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
@@ -52,6 +82,18 @@ export default function App() {
   const postArticle = article => {
     // ✨ implement
     // The flow is very similar to the `getArticles` function.
+    setMessage("");
+    setSpinnerOn(true);
+    axios.post(articlesUrl, article, { headers: { Authorization: localStorage.getItem("token") }})
+      .then((response) => {
+        setMessage(response.data.message);
+        const article = response.data.article;
+        setArticles([ ...articles, article ]);
+      })
+      .catch((error) => {
+        console.log("post error: ", error)
+      })
+      .finally(() => setSpinnerOn(false))
     // You'll know what to do! Use log statements or breakpoints
     // to inspect the response from the server.
   }
@@ -59,17 +101,48 @@ export default function App() {
   const updateArticle = ({ article_id, article }) => {
     // ✨ implement
     // You got this!
+    setMessage("");
+    setSpinnerOn(true);
+    axios.put(`${articlesUrl}/${article_id}`, article, { headers: { Authorization: localStorage.getItem("token") }})
+      .then((response) => {
+        setMessage(response.data.message);
+        const newArticles = articles.map((art) => {
+          if (art.article_id === article_id) {
+            return { ...article, article_id }
+          } 
+          return art;
+        })
+        setArticles(newArticles);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setSpinnerOn(false))
   }
 
   const deleteArticle = article_id => {
     // ✨ implement
+    setMessage("");
+    setSpinnerOn(true);
+    axios.delete(`${articlesUrl}/${article_id}`, { headers: { Authorization: localStorage.getItem("token") }})
+      .then((response) => {
+        setMessage(response.data.message);
+        const filteredArticles = articles.filter((art) => {
+          if (art.article_id !== article_id) return art;
+        })
+        setArticles(filteredArticles);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setSpinnerOn(false))
   }
 
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
-      <Spinner />
-      <Message />
+      <Spinner on={spinnerOn} />
+      <Message message={message} />
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +151,22 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login} />} />
           <Route path="articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm 
+                postArticle={postArticle} 
+                updateArticle={updateArticle} 
+                setCurrentArticleId={setCurrentArticleId}
+                currentArticle={articles.find(articles => articles.article_id === currentArticleId)}
+              />
+              <Articles 
+                articles={articles} 
+                getArticles={getArticles} 
+                deleteArticle={deleteArticle} 
+                currentArticleId={currentArticleId} 
+                setCurrentArticleId={setCurrentArticleId} 
+              />
             </>
           } />
         </Routes>
--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -2,15 +2,18 @@ import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import PT from 'prop-types'
 
-export default function Articles(props) {
+export default function Articles({ articles, getArticles, deleteArticle, currentArticleId, setCurrentArticleId }) {
   // ✨ where are my props? Destructure them here
 
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
+  const token = localStorage.getItem("token");
+  if (!token) return <Navigate to="/" />
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
-  })
+    getArticles();
+  }, []);
 
   return (
     // ✨ fix the JSX: replace `Function.prototype` with actual functions
@@ -18,9 +21,9 @@ export default function Articles(props) {
     <div className="articles">
       <h2>Articles</h2>
       {
-        ![].length
+        articles.length === 0
           ? 'No articles yet'
-          : [].map(art => {
+          : articles.map(art => {
             return (
               <div className="article" key={art.article_id}>
                 <div>
@@ -29,8 +32,8 @@ export default function Articles(props) {
                   <p>Topic: {art.topic}</p>
                 </div>
                 <div>
-                  <button disabled={true} onClick={Function.prototype}>Edit</button>
-                  <button disabled={true} onClick={Function.prototype}>Delete</button>
+                  <button disabled={!!currentArticleId} onClick={() => setCurrentArticleId(art.article_id)}>Edit</button>
+                  <button disabled={!!currentArticleId} onClick={() => deleteArticle(art.article_id)}>Delete</button>
                 </div>
               </div>
             )
